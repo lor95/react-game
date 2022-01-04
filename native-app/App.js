ac2805e4eac3b16706fc27aa671855ebcd1628e4
ubuntu <lory_extr3me@hotmail.it>
2022-01-04 12:10:44 +0100

    commit fix

--- native-app/App.js
+++ native-app/App.js
@@ -1,6 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  BoxBufferGeometry,
+} from "three";
+import { Renderer } from "expo-three";
+import { GLView } from "expo-gl";
 import { StatusBar } from "expo-status-bar";
-import { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
 import socketIoClient from "socket.io-client";
 const ENDPOINT = "ws://192.168.1.220:4001";
 
@@ -26,20 +35,49 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       {Boolean(error) && <Text style={{ color: "red" }}>{error}</Text>}
       {!Boolean(error) && <Text>{connMessage}</Text>}
       <Text>{response}</Text>
-      <StatusBar style="auto" />
+      <GLView
+        style={{ flex: 1 }}
+        onContextCreate={(gl) => {
+          // Create a WebGLRenderer without a DOM element
+          var scene = new Scene();
+          // 2. Camera
+          const camera = new PerspectiveCamera(
+            75,
+            gl.drawingBufferWidth / gl.drawingBufferHeight,
+            0.1,
+            1000
+          );
+          try {
+            gl.canvas = {
+              width: gl.drawingBufferWidth,
+              height: gl.drawingBufferHeight,
+            };
+          } catch {}
+          const renderer = new Renderer({ gl });
+          renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+          const geometry = new BoxBufferGeometry(1, 1, 1);
+          const material = new MeshBasicMaterial({ color: 'lightblue' });
+          const cube = new Mesh(geometry, material);
+          scene.add(cube);
+
+          camera.position.z = 5;
+
+          //scene.add(cube);
+
+          const animate = () => {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+            gl.endFrameEXP();
+          };
+          animate();
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
