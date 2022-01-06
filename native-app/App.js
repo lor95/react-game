aa2639d25a2519e1966529a81907247e68461f88
ubuntu <lory_extr3me@hotmail.it>
2022-01-06 19:54:37 +0100

    texture + improvements

--- native-app/App.js
+++ native-app/App.js
@@ -7,18 +7,28 @@ import {
   PerspectiveCamera,
   BoxBufferGeometry,
   GridHelper,
+  DirectionalLight,
+  HemisphereLight,
+  MeshLambertMaterial,
   SpotLight,
+  TextureLoader,
+  RepeatWrapping,
+  sRGBEncoding,
+  PlaneBufferGeometry,
+  PCFShadowMap,
   Fog,
+  Color,
 } from "three";
 import { Renderer } from "expo-three";
 import { GLView } from "expo-gl";
-import gsap from "gsap";
+//import gsap from "gsap";
 import socketIoClient from "socket.io-client";
 const ENDPOINT = "ws://192.168.1.220:4001";
 
 let keys = {};
 let brakeEngineTimeouts = {};
 let speed = { x: 0, z: 0, y: 0 };
+let expectedTurn = 0;
 const availableColors = ["red", "cyan", "lightblue", "lime", "green", "orange"];
 const color =
   availableColors[Math.floor(Math.random() * availableColors.length)];
@@ -30,6 +40,7 @@ const player = new Mesh(geometry, material);
 const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
 
 export default function App() {
+  const image = require("./models/ground.png");
   const [error, setError] = useState(undefined);
   const [socketId, setSocketId] = useState(undefined);
   const [socket, setSocket] = useState(undefined);
@@ -65,8 +76,11 @@ export default function App() {
         const alreadySpawnedPlayer = new Mesh(geometry, material);
         alreadySpawnedPlayer.socketId = socketId;
         alreadySpawnedPlayer.position.x = player.position.x;
+        alreadySpawnedPlayer.position.y = 0.25;
         alreadySpawnedPlayer.position.z = player.position.z;
         alreadySpawnedPlayer.rotation.y = player.rotation.y;
+        alreadySpawnedPlayer.castShadow = true;
+        alreadySpawnedPlayer.receiveShadow = true;
         scene.add(alreadySpawnedPlayer);
       });
     });
@@ -78,8 +92,11 @@ export default function App() {
         const spawnedPlayer = new Mesh(geometry, material);
         spawnedPlayer.socketId = player.socketId;
         spawnedPlayer.position.x = player.position.x;
+        spawnedPlayer.position.y = 0.25;
         spawnedPlayer.position.z = player.position.z;
         spawnedPlayer.rotation.y = player.rotation.y;
+        spawnedPlayer.castShadow = true;
+        spawnedPlayer.receiveShadow = true;
         scene.add(spawnedPlayer);
       }
     });
@@ -113,32 +130,26 @@ export default function App() {
   const movePlayer = (data) => {
     const latestPosition = player.position;
     const latestRotation = player.rotation;
-    gsap.to(player.position, {
-      duration: 0.04,
-      x: player.position.x + (data.x || 0),
-      z: player.position.z + (data.z || 0),
-      onComplete: () => {
-        if (
-          data.x !== latestPosition.x ||
-          data.z !== latestPosition.z ||
-          data.y !== latestRotation.y
-        )
-          socket.emit("player_move", {
-            position: {
-              x: player.position.x,
-              z: player.position.z,
-            },
-            rotation: {
-              y: player.rotation.y,
-            },
-          });
-      },
-    });
-    gsap.to(camera.position, {
-      duration: 0.04,
-      x: camera.position.x + (data.x || 0),
-      z: camera.position.z + (data.z || 0),
-    });
+    player.position.x += data.x || 0;
+    player.position.z += data.z || 0;
+    player.rotation.y += data.y || 0;
+    if (
+      data.x !== latestPosition.x ||
+      data.z !== latestPosition.z ||
+      data.y !== latestRotation.y
+    ) {
+      socket.emit("player_move", {
+        position: {
+          x: player.position.x,
+          z: player.position.z,
+        },
+        rotation: {
+          y: player.rotation.y,
+        },
+      });
+    }
+    camera.position.x += data.x || 0;
+    camera.position.z += data.z || 0;
   };
 
   const getCodeInfo = (code) => {
@@ -200,26 +211,31 @@ export default function App() {
 
   const moveLogic = () => {
     if (Platform.OS === "web") {
-      const accCoeff = 0.8;
-      const brakeEngine = 0.35;
-      const topSpeed = 0.26;
-      const brakeCoeff = 0.95;
-      const reverseSpeed = 0.08;
-      const steeringCoeff = 0.03;
+      const accCoeff = 1.2;
+      const brakeEngine = 0.65;
+      const topSpeed = 0.35;
+      const brakeCoeff = 1.9;
+      const reverseSpeed = 0.1;
+      const steeringCoeff = 0.05;
 
       let execMove = false;
 
       if (keys["ArrowLeft"]?.pressed && keys["ArrowLeft"]?.type === "keydown") {
-        player.rotation.y += steeringCoeff;
-      } else if (
+        if (expectedTurn + steeringCoeff <= 0.1) expectedTurn += steeringCoeff;
+      }
+      if (
         keys["ArrowRight"]?.pressed &&
         keys["ArrowRight"]?.type === "keydown"
       ) {
-        player.rotation.y -= steeringCoeff;
+        if (expectedTurn - steeringCoeff >= -0.1) expectedTurn -= steeringCoeff;
+      }
+      if (keys["ArrowUp"]?.pressed || keys["ArrowDown"]?.pressed) {
+        speed.y = expectedTurn;
+        expectedTurn = 0;
       }
 
-      const sinAngle = Math.sin(player.rotation.y);
-      const cosAngle = Math.cos(player.rotation.y);
+      const sinAngle = Math.sin(player.rotation.y + speed.y);
+      const cosAngle = Math.cos(player.rotation.y + speed.y);
       let angleQuadrant;
       if (cosAngle > 0 && sinAngle > 0) angleQuadrant = 1;
       else if (cosAngle < 0 && sinAngle > 0) angleQuadrant = 2;
@@ -396,10 +412,10 @@ export default function App() {
       //  `actual speed: ${Math.sqrt(speed.x * speed.x + speed.z * speed.z)}`
       //);
       console.log(speed);
-      console.log(keys["ArrowUp"]);
+      //console.log(keys["ArrowUp"]);
       //console.log(keys["ArrowLeft"]);
       //console.log(keys["ArrowRight"]);
-      if (execMove) movePlayer({ ...speed, y: player.position.y });
+      if (execMove) movePlayer(speed);
     }
   };
 
@@ -410,23 +426,39 @@ export default function App() {
         <GLView
           style={{ flex: 1 }}
           onContextCreate={(gl) => {
-            scene.add(new GridHelper(10000, 10000));
+            scene.fog = new Fog("#87ceeb", 1, 30);
+            scene.background = new Color("#87ceeb");
+            console.log(require("./models/ground.png"));
+            const groundTexture = new TextureLoader().load(
+              require("./models/ground.png")
+            );
+            groundTexture.wrapS = groundTexture.wrapT = RepeatWrapping;
+            groundTexture.repeat.set(10000, 10000);
+            groundTexture.anisotropy = 16;
+            groundTexture.encoding = sRGBEncoding;
+            const groundMaterial = new MeshStandardMaterial({
+              map: groundTexture,
+            });
+            const groundMesh = new Mesh(
+              new PlaneBufferGeometry(10000, 10000),
+              groundMaterial
+            );
+            groundMesh.position.y = 0;
+            groundMesh.rotation.x = -Math.PI / 2;
+            groundMesh.receiveShadow = true;
+            scene.add(groundMesh);
 
-            scene.fog = new Fog("#3A96C4", 1, 10);
+            var hemilight = new HemisphereLight(0xffeeb1, 0x080820, 1);
+            scene.add(hemilight);
+            var light = new SpotLight(0xffa95c, 1);
+            light.position.set(-50, 50, 50);
+            light.castShadow = true;
+            light.shadow.bias = 0.001;
+            light.shadow.mapSize.width = 512;
+            light.shadow.mapSize.height = 512;
+            scene.add(light);
+            //light.position.set(-400, 1500, -1000);
 
-            const spotLight = new SpotLight(0xffffff);
-            spotLight.position.set(-1000, 1000, -1000);
-
-            //spotLight.castShadow = true;
-
-            spotLight.shadow.mapSize.width = 1024;
-            spotLight.shadow.mapSize.height = 1024;
-
-            spotLight.shadow.camera.near = 500;
-            spotLight.shadow.camera.far = 4000;
-            spotLight.shadow.camera.fov = 30;
-
-            scene.add(spotLight);
             try {
               gl.canvas = {
                 width: gl.drawingBufferWidth,
@@ -436,6 +468,8 @@ export default function App() {
             const renderer = new Renderer({ gl });
 
             renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+            renderer.shadowMap.enabled = true;
+            renderer.shadowMap.type = PCFShadowMap;
             camera.aspect = gl.drawingBufferWidth / gl.drawingBufferHeight;
             camera.updateProjectionMatrix();
 
@@ -445,12 +479,12 @@ export default function App() {
             const initZ =
               (Math.round(Math.random()) * 2 - 1) *
               Math.floor(Math.random() * 5);
-
             player.socketId = socketId;
-            player.position.set(initX, 0, initZ);
+            player.position.set(initX, 0.25, initZ);
             camera.position.set(initX, 2, initZ - 5);
-            player.rotation.y = 0;
             camera.lookAt(player.position);
+            player.castShadow = true;
+            player.receiveShadow = true;
             scene.add(player);
             playerInit(player.position, player.rotation, color);
 
