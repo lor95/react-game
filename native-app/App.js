673b762c6f574c46a34970b45ed78f0202500dba
lor95 <lory_extr3me@hotmail.it>
2022-01-16 21:33:18 +0100

    simplegamewindow

--- native-app/App.js
+++ native-app/App.js
@@ -1,35 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { SimpleGameWindow } from "./lib/physic-engine-three/components";
 import { SOCKET_ENDPOINT } from "./config/socketConfig";
-import { View, Text, Platform, TouchableWithoutFeedback } from "react-native";
-import {
-  Scene,
-  Mesh,
-  MeshStandardMaterial,
-  GridHelper,
-  HemisphereLight,
-  SpotLight,
-  RepeatWrapping,
-  sRGBEncoding,
-  PlaneBufferGeometry,
-  PCFShadowMap,
-  Fog,
-  Color,
-  Quaternion,
-  BoxGeometry,
-} from "three";
-import {
-  World,
-  Body,
-  Box,
-  Vec3,
-  Plane,
-  Material,
-  ContactMaterial,
-} from "cannon";
-import { default as CannonDebugRenderer } from "./lib/physic-engine-three/debug/CannonDebugRenderer";
+import { View, Text, TouchableWithoutFeedback } from "react-native";
+import { Scene, Quaternion } from "three";
+import { World, Vec3 } from "cannon";
 import { SimpleCarObject } from "./lib/physic-engine-three/core";
-import { Renderer, TextureLoader } from "expo-three";
-import { GLView } from "expo-gl";
 import socketIoClient from "socket.io-client";
 
 const defaultColors = [
@@ -41,12 +16,8 @@ const defaultColors = [
   "#ff6666",
 ];
 
-const debug = false;
-const randomBoxes = true;
-
 const scene = new Scene();
 const world = new World();
-let players = [];
 
 const player = new SimpleCarObject(
   {
@@ -59,6 +30,8 @@ const player = new SimpleCarObject(
   true,
   true
 );
+
+let players = [];
 
 export default function App() {
   const [error, setError] = useState(undefined);
@@ -73,6 +46,12 @@ export default function App() {
     });
     setSocket(socket_);
   }, []);
+
+  useEffect(() => {
+    if (Boolean(socketId)) {
+      player.setCommonId(socketId);
+    }
+  }, [socketId]);
 
   if (Boolean(socket) && !socket.connected) {
     socket.on("connect_error", () => {
@@ -170,172 +149,28 @@ export default function App() {
     <View style={{ flex: 1 }}>
       {Boolean(error) && <Text style={{ color: "red" }}>{error}</Text>}
       {Boolean(socket) && Boolean(socketId) && socket.connected && (
-        <GLView
-          style={{ flex: 1 }}
-          onContextCreate={(gl) => {
-            world.quatNormalizeSkip = 0;
-            world.quatNormalizeFast = false;
-            world.defaultContactMaterial.contactEquationStiffness = 1e128;
-            world.defaultContactMaterial.contactEquationRelaxation = 4;
-            world.gravity.set(0, -9.82, 0);
-            world.solver.iterations = 20;
-            world.solver.tolerance = 0.0;
-
-            // Create a slippery material (friction coefficient = 0.0)
-            var physicsMaterial = new Material("slipperyMaterial");
-            var physicsContactMaterial = new ContactMaterial(
-              physicsMaterial,
-              physicsMaterial,
-              0.0, // friction coefficient
-              0.3 // restitution
-            );
-            world.addContactMaterial(physicsContactMaterial);
-
-            // Create a plane
-            const groundShape = new Plane();
-            const groundBody = new Body({ mass: 0 });
-            groundBody.addShape(groundShape);
-            groundBody.quaternion.setFromAxisAngle(
-              new Vec3(1, 0, 0),
-              -Math.PI / 2
-            );
-            world.addBody(groundBody);
-
-            let boxes = [];
-            let boxMeshes = [];
-            if (randomBoxes) {
-              var boxShape = new Box(new Vec3(1, 1, 1));
-              for (var i = 0; i < 7; i++) {
-                var x = (Math.random() - 0.5) * 30;
-                var y = 1 + (Math.random() - 0.5) * 1;
-                var z = (Math.random() - 0.5) * 30;
-                var boxBody = new Body({ mass: 0.1 });
-                boxBody.addShape(boxShape);
-                var boxMesh = new Mesh(
-                  new BoxGeometry(2, 2, 2),
-                  new MeshStandardMaterial({ color: "blue" })
-                );
-                world.addBody(boxBody);
-                scene.add(boxMesh);
-                boxBody.position.set(x, y, z);
-                boxMesh.position.set(x, y, z);
-                boxMesh.castShadow = true;
-                boxMesh.receiveShadow = true;
-                boxes.push(boxBody);
-                boxMeshes.push(boxMesh);
-              }
-            }
-
-            var hemilight = new HemisphereLight(0xffeeb1, 0x080820, 1);
-            scene.add(hemilight);
-            var light = new SpotLight(0xffa95c, 1);
-            light.position.set(-50, 50, 50);
-            light.castShadow = true;
-            light.shadow.bias = 0.001;
-            light.shadow.mapSize.width = 512;
-            light.shadow.mapSize.height = 512;
-            scene.add(light);
-
-            try {
-              gl.canvas = {
-                width: gl.drawingBufferWidth,
-                height: gl.drawingBufferHeight,
-              };
-            } catch {}
-
-            const renderer = new Renderer({ gl });
-            let debugRenderer;
-
-            renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-            renderer.shadowMap.enabled = true;
-            renderer.shadowMap.type = PCFShadowMap;
-
-            player.camera.aspect =
-              gl.drawingBufferWidth / gl.drawingBufferHeight;
-            player.camera.updateProjectionMatrix();
-
-            scene.add(player.chassisShape);
-            player.wheelShapes.forEach((wheelShape) => {
-              scene.add(wheelShape);
+        <SimpleGameWindow
+          debugWindow={true}
+          mainPlayer={player}
+          mainPlayerInit={playerInit}
+          randomBoxes={false}
+          scene={scene}
+          updateMainPlayer={() => {
+            socket.emit("player_move", {
+              position: {
+                x: player.chassisShape.position.x,
+                y: player.chassisShape.position.y,
+                z: player.chassisShape.position.z,
+              },
+              quaternion: {
+                _x: player.chassisShape.quaternion._x,
+                _y: player.chassisShape.quaternion._y,
+                _z: player.chassisShape.quaternion._z,
+                _w: player.chassisShape.quaternion._w,
+              },
             });
-            player.addToWorld(world);
-            player.wheelBodies.forEach((wheel) => {
-              world.addBody(wheel);
-            });
-            player.setCommonId(socketId);
-
-            player.enableBrowserStdControls();
-
-            playerInit(
-              player.chassisShape.position,
-              player.chassisShape.quaternion,
-              player.chassisShape.material.color
-            );
-
-            if (debug) {
-              scene.add(new GridHelper(1000, 1000));
-              scene.add(player.forwardArrow);
-              scene.add(player.velocityArrow);
-              debugRenderer = new CannonDebugRenderer(scene, world); // debug element
-            } else {
-              scene.fog = new Fog("#87ceeb", 1, 30);
-              scene.background = new Color("#87ceeb");
-              const groundTexture = new TextureLoader().load(
-                require("./resources/textures/ground.png")
-              );
-              groundTexture.wrapS = RepeatWrapping;
-              groundTexture.wrapT = RepeatWrapping;
-              groundTexture.repeat.set(10000, 10000);
-              groundTexture.anisotropy = 16;
-              groundTexture.encoding = sRGBEncoding;
-              const groundMaterial = new MeshStandardMaterial({
-                map: groundTexture,
-              });
-              const groundMesh = new Mesh(
-                new PlaneBufferGeometry(10000, 10000),
-                groundMaterial
-              );
-              groundMesh.position.y = 0;
-              groundMesh.rotation.x = -Math.PI / 2;
-              groundMesh.receiveShadow = true;
-              scene.add(groundMesh);
-            }
-
-            const animate = () => {
-              setTimeout(function () {
-                requestAnimationFrame(animate);
-                world.step(1 / 30);
-              }, 1000 / 30);
-              player.updatePosition(() => {
-                socket.emit("player_move", {
-                  position: {
-                    x: player.chassisShape.position.x,
-                    y: player.chassisShape.position.y,
-                    z: player.chassisShape.position.z,
-                  },
-                  quaternion: {
-                    _x: player.chassisShape.quaternion._x,
-                    _y: player.chassisShape.quaternion._y,
-                    _z: player.chassisShape.quaternion._z,
-                    _w: player.chassisShape.quaternion._w,
-                  },
-                });
-              });
-
-              if (randomBoxes) {
-                for (var i = 0; i < boxes.length; i++) {
-                  boxMeshes[i].position.copy(boxes[i].position);
-                  boxMeshes[i].quaternion.copy(boxes[i].quaternion);
-                }
-              }
-
-              Boolean(debugRenderer) && debugRenderer.update();
-
-              renderer.render(scene, player.camera);
-              gl.endFrameEXP();
-            };
-            animate();
           }}
+          world={world}
         />
       )}
     </View>
---
+++ native-app/lib/physic-engine-three/components/index.js
@@ -0,0 +1,1 @@
+export { default as SimpleGameWindow } from "./SimpleGameWindow";
