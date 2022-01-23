f3fc5b133d172e96c3e76270d9c17abf85ccd086
lor95 <lory_extr3me@hotmail.it>
2022-01-23 10:54:17 +0100

    ios application fix - it now builds

--- native-app/config/socketConfig.js
+++ native-app/config/socketConfig.js
@@ -1,2 +1,2 @@
-export const SOCKET_ENDPOINT = "ws://192.168.1.220:4001"; // ubuntu vm
-//export const SOCKET_ENDPOINT = "ws://192.168.1.79:4001"; // macos
+//export const SOCKET_ENDPOINT = "ws://192.168.1.220:4001"; // ubuntu vm
+export const SOCKET_ENDPOINT = "ws://192.168.1.79:4001"; // macos
